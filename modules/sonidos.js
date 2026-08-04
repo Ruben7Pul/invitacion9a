ba783e63750d7e2e8fdfcaf4f38663684faea70f
Ruben7Pul <arturo9angel7@outlook.com>
2026-08-04 17:31:41 -0600

    Update sonidos.js

--- modules/sonidos.js
+++ modules/sonidos.js
@@ -51,7 +51,14 @@ function preloadSounds() {
     win: { freqs: [784, 988, 1175, 1568], dur: 0.6 },
     clay: { freqs: [600, 800], dur: 0.15 },
     wood: { freqs: [400, 500, 300], dur: 0.25 },
-    iron: { freqs: [200, 250, 300, 350], dur: 0.3 }
+    iron: { freqs: [200, 250, 300, 350], dur: 0.3 },
+    powerup_good: { freqs: [988, 1318, 1760], dur: 0.3 },
+    powerup_bad: { freqs: [440, 370, 330], dur: 0.4 },
+    extra_life: { freqs: [880, 1175, 1568, 2093], dur: 0.5 },
+    blue_ball: { freqs: [523, 659, 784, 988, 1175], dur: 0.6 },
+    wall_hit: { freqs: [880, 660], dur: 0.1 },
+    game_over: { freqs: [440, 370, 330, 294], dur: 0.6 },
+    paddle_hit: { freqs: [1200, 900], dur: 0.08 }
   };
   for (const [name, def] of Object.entries(soundDefs)) {
     soundBuffers[name] = createSoundBuffer(ctx, def.freqs, def.dur);
@@ -85,6 +92,13 @@ export const soundWin = () => playSound('win');
 export const soundClay = () => playSound('clay');
 export const soundWood = () => playSound('wood');
 export const soundIron = () => playSound('iron');
+export const soundPowerupGood = () => playSound('powerup_good');
+export const soundPowerupBad = () => playSound('powerup_bad');
+export const soundExtraLife = () => playSound('extra_life');
+export const soundBlueBall = () => playSound('blue_ball');
+export const soundWallHit = () => playSound('wall_hit');
+export const soundGameOver = () => playSound('game_over');
+export const soundPaddleHit = () => playSound('paddle_hit');
 
 export function initSonidos() {
   document.addEventListener('click', () => {
