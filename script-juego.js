84da290b15194750bd9658f9335fb445da92f34a
Ruben7Pul <arturo9angel7@outlook.com>
2026-08-06 12:30:56 -0600

    Update script-juego.js

--- script-juego.js
+++ script-juego.js
@@ -36,7 +36,6 @@ async function cargarConfig() {
     const config = await cargarConfig();
     // Cargar música con ruta específica para el juego
     const { initMusica, playMusic, toggleMusic, isMusicMuted } = await import('./modules/musica.js');
-    // Forzar ruta a juegcan.mp3
     initMusica('../archivos/juegcan.mp3');
     window.toggleMusic = toggleMusic;
     window.isMusicMuted = isMusicMuted;
